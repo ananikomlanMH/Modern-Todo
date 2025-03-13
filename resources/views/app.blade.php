d8d160733f6ce29efee8938682800c419c9f7104
Anani Komlan Mawulom Hounkpati <ananikmh17@gmail.com>
2025-03-13 19:07:25 +0100

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
         <meta name="title" content="Todo App - Gérez vos tâches efficacement">
         <meta name="description" content="Application de gestion de tâches moderne construite avec Laravel et React">
 
